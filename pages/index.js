746adf67e50239de95f805b22ae9bcf65999bd9c
Kareem2002Shimes <kareem4shimas@gmail.com>
2023-04-07 00:39:48 +0200

    tst

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,8 @@ import { Fragment } from "react";
 import ProductList from "../components/products/ProductList";
 import axios from "axios";
 import Head from "next/head";
+import { dbConnect } from "../util/mongo";
+import Product from "../models/Product";
 
 function HomePage(props) {
   return (
@@ -21,12 +23,15 @@ export default HomePage;
 
 export async function getServerSideProps() {
   // fetch data from an API
-  const res = await axios.get(
-    `${process.env.APP_DEV || process.env.APP_PROD}/api/products`
-  );
+  // const res = await axios.get(
+  //   `${process.env.APP_DEV || process.env.APP_PROD}/api/products`
+  // );
+  dbConnect();
+  const products = await Product.find();
+
   return {
     props: {
-      products: res.data.data.map((product) => ({
+      products: products.map((product) => ({
         image: product.image,
         name: product.name,
         price: product.price,
